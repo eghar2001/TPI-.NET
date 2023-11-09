bad6d1fea9dfc0ab34dab6ae8d7f4e6fae486c7f
Nahuel Coronel <nahucarc2@gmail.com>
2023-11-09 20:05:34 -0300

    gitignore actualizado

--- untrack_files.py
+++ untrack_files.py
@@ -1,6 +1,6 @@
 from os import system
 
-with open("eliminar.txt", mode = "r") as archivo:
+with open(".gitignore", mode = "r") as archivo:
     for linea in archivo:
         if not linea.startswith("#"):
             system(f"git rm -r --cached {linea}")
